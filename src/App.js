34e68a565c06d764b66a2af7fd93f44185ab685c
김동영 <kimdong-yeong@gimdong-yeong-ui-MacBookAir.local>
2022-06-25 16:35:16 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 function App() {
   return (
     <div>
-      <MainPageComponent />
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
     </div>
   );
 }

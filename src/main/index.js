f4b878f39c48f9faec259ddb96f166f211332325
김동영 <kimdong-yeong@gimdong-yeong-ui-MacBookAir.local>
2022-07-10 21:01:33 +0900

    antd 기본적용

--- src/main/index.js
+++ src/main/index.js
@@ -11,6 +11,7 @@ function MainPage() {
         "https://f188ff6a-854e-40db-8dd4-693360e4ff88.mock.pstmn.io/products"
       )
       .then(function (result) {
+        //console.log("데이터전송 성공:", result);
         const products = result.data.products;
         setProducts(products);
       })
@@ -18,45 +19,37 @@ function MainPage() {
         console.error("에러발생 :   ", error);
       });
   }, []);
+
   return (
     <div>
-      <div id="header">
-        <div id="header-area">
-          <img src="images/icons/logo.png" />
-        </div>
+      <div id="banner">
+        <img src="images/banners/banner1.png" />
       </div>
-      <div id="body">
-        <div id="banner">
-          <img src="images/banners/banner1.png" />
-        </div>
-        <h1>판매되는 상품들</h1>
-        <div id="product-list">
-          {products.map(function (product, index) {
-            return (
-              <div className="product-card">
-                <Link className="product-link" to={"/products/" + index}>
-                  <div>
-                    <img className="product-img" src={product.imageUrl} />
+      <h1>판매되는 상품들</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link className="product-link" to={`/products/${product.id}`}>
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
                   </div>
-                  <div className="product-contents">
-                    <span className="product-name">{product.name}</span>
-                    <span className="product-price">{product.price}원</span>
-                    <div className="product-seller">
-                      <img
-                        className="product-avatar"
-                        src="images/icons/avatar.png"
-                      />
-                      <span>{product.seller}</span>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-          ;
-        </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
       </div>
-      <div id="footer"></div>
     </div>
   );
 }
